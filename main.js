81b22bdd07ef63edf9c569b7dec54dce1849066e
Danil Murzin <danil.murzin.200026@gmail.com>
2022-08-22 22:51:09 +0300

    fix move selector

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 'use strict';
-
+document.body.style.overflow = 'hidden';
 const DomElement = function (selector, height, width, bg, fontSize, position) {
   this.selector = selector;
   this.height = height;
@@ -25,6 +25,47 @@ DomElement.prototype.createSelector = function () {
   elem.style.cssText = `height: ${this.height}; width: ${this.width}; background-color: ${this.bg}; font-size: ${this.fontSize}; position: ${this.position}`;
 };
 
+DomElement.prototype.moveSelector = function () {
+  document.addEventListener('DOMContentLoaded', () => {
+    const elem = document.querySelector('.block');
+
+    document.addEventListener('keydown', function (event) {
+      if (
+        event.code != 'ArrowRight' &&
+        event.code != 'ArrowLeft' &&
+        event.code != 'ArrowUp' &&
+        event.code != 'ArrowDown'
+      )
+        return;
+
+      let rectElem = elem.getBoundingClientRect();
+
+      let x = rectElem.x + pageXOffset,
+        y = rectElem.y + pageYOffset;
+
+      if (event.code == 'ArrowRight') x += 10;
+      if (event.code == 'ArrowLeft') x -= 10;
+      if (event.code == 'ArrowUp') y -= 10;
+      if (event.code == 'ArrowDown') y += 10;
+
+      elem.style.left = x + 'px';
+      elem.style.top = y + 'px';
+
+      if (x > window.innerWidth) {
+        elem.style.left = 0 + 'px';
+      } else if (x < -100) {
+        elem.style.left = window.innerWidth + 'px';
+      }
+
+      if (y > window.innerHeight) {
+        elem.style.top = 0 + 'px';
+      } else if (y < -100) {
+        elem.style.top = window.innerHeight + 'px';
+      }
+    });
+  });
+};
+
 const test = new DomElement(
   '.block',
   '100px',
@@ -34,32 +75,7 @@ const test = new DomElement(
   'absolute'
 );
 
-console.log(test);
+// console.log(test2);
 console.log(test.createSelector());
-
-document.addEventListener('DOMContentLoaded', () => {
-  const elem = document.querySelector('.block');
-
-  document.addEventListener('keydown', function (event) {
-    if (
-      event.code != 'ArrowRight' &&
-      event.code != 'ArrowLeft' &&
-      event.code != 'ArrowUp' &&
-      event.code != 'ArrowDown'
-    )
-      return;
-
-    let rectElem = elem.getBoundingClientRect();
-
-    let x = rectElem.x + pageXOffset,
-      y = rectElem.y + pageYOffset;
-
-    if (event.code == 'ArrowRight') x += 10;
-    if (event.code == 'ArrowLeft') x -= 10;
-    if (event.code == 'ArrowUp') y -= 10;
-    if (event.code == 'ArrowDown') y += 10;
-
-    elem.style.left = x + 'px';
-    elem.style.top = y + 'px';
-  });
-});
+console.log(test.moveSelector());
+// console.log(test2.createSelector());
